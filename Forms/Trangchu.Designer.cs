716e73b482de45588aa54e6ccfd9f0faaa4fc217
giahuy <boomhuyxt@gmail.com>
2025-11-16 01:29:09 +0700

    cập nhật trang chủ

--- Forms/Trangchu.Designer.cs
+++ Forms/Trangchu.Designer.cs
@@ -28,26 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Trangchu));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.Taskbar = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_future = new System.Windows.Forms.Button();
+            this.btnGiaoDich = new System.Windows.Forms.Button();
             this.btn_thịtruong = new System.Windows.Forms.Button();
-            this.Cbb_Giaodich = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuGiaoDich = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.p2pToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.spotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.marginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel_Thitruong = new System.Windows.Forms.Panel();
+            this.panel_future = new System.Windows.Forms.Panel();
+            this.dgv_Thitruong = new System.Windows.Forms.DataGridView();
             this.Taskbar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuGiaoDich.SuspendLayout();
+            this.panel_future.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Thitruong)).BeginInit();
             this.SuspendLayout();
             // 
             // Taskbar
             // 
-            this.Taskbar.Controls.Add(this.button1);
-            this.Taskbar.Controls.Add(this.Cbb_Giaodich);
+            this.Taskbar.Controls.Add(this.btn_future);
+            this.Taskbar.Controls.Add(this.btnGiaoDich);
             this.Taskbar.Controls.Add(this.btn_thịtruong);
             this.Taskbar.Controls.Add(this.pictureBox1);
             this.Taskbar.Location = new System.Drawing.Point(2, 1);
             this.Taskbar.Name = "Taskbar";
             this.Taskbar.Size = new System.Drawing.Size(798, 38);
             this.Taskbar.TabIndex = 0;
+            // 
+            // btn_future
+            // 
+            this.btn_future.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
+            this.btn_future.FlatAppearance.BorderSize = 0;
+            this.btn_future.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_future.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_future.ForeColor = System.Drawing.Color.White;
+            this.btn_future.Location = new System.Drawing.Point(228, 9);
+            this.btn_future.Name = "btn_future";
+            this.btn_future.Size = new System.Drawing.Size(75, 23);
+            this.btn_future.TabIndex = 9;
+            this.btn_future.Text = "Future";
+            this.btn_future.UseVisualStyleBackColor = false;
+            // 
+            // btnGiaoDich
+            // 
+            this.btnGiaoDich.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
+            this.btnGiaoDich.FlatAppearance.BorderSize = 0;
+            this.btnGiaoDich.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGiaoDich.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiaoDich.ForeColor = System.Drawing.Color.White;
+            this.btnGiaoDich.Location = new System.Drawing.Point(151, 9);
+            this.btnGiaoDich.Name = "btnGiaoDich";
+            this.btnGiaoDich.Size = new System.Drawing.Size(91, 27);
+            this.btnGiaoDich.TabIndex = 8;
+            this.btnGiaoDich.Text = "Giao dịch▾";
+            this.btnGiaoDich.UseVisualStyleBackColor = false;
+            this.btnGiaoDich.Click += new System.EventHandler(this.btnGiaoDich_Click);
+            // 
+            // btn_thịtruong
+            // 
+            this.btn_thịtruong.FlatAppearance.BorderSize = 0;
+            this.btn_thịtruong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_thịtruong.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_thịtruong.ForeColor = System.Drawing.Color.White;
+            this.btn_thịtruong.Location = new System.Drawing.Point(71, 9);
+            this.btn_thịtruong.Name = "btn_thịtruong";
+            this.btn_thịtruong.Size = new System.Drawing.Size(86, 26);
+            this.btn_thịtruong.TabIndex = 7;
+            this.btn_thịtruong.Text = "Thị trường";
+            this.btn_thịtruong.UseVisualStyleBackColor = false;
+            this.btn_thịtruong.Click += new System.EventHandler(this.btn_thịtruong_Click);
             // 
             // pictureBox1
             // 
@@ -60,49 +116,89 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // btn_thịtruong
+            // menuGiaoDich
             // 
-            this.btn_thịtruong.FlatAppearance.BorderSize = 0;
-            this.btn_thịtruong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_thịtruong.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_thịtruong.ForeColor = System.Drawing.Color.White;
-            this.btn_thịtruong.Location = new System.Drawing.Point(71, 12);
-            this.btn_thịtruong.Name = "btn_thịtruong";
-            this.btn_thịtruong.Size = new System.Drawing.Size(75, 23);
-            this.btn_thịtruong.TabIndex = 7;
-            this.btn_thịtruong.Text = "Thị trường";
-            this.btn_thịtruong.UseVisualStyleBackColor = false;
-            this.btn_thịtruong.Click += new System.EventHandler(this.btn_thịtruong_Click);
+            this.menuGiaoDich.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
+            this.menuGiaoDich.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.menuGiaoDich.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.p2pToolStripMenuItem,
+            this.spotToolStripMenuItem,
+            this.marginToolStripMenuItem});
+            this.menuGiaoDich.Name = "menuGiaoDich";
+            this.menuGiaoDich.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuGiaoDich.ShowImageMargin = false;
+            this.menuGiaoDich.Size = new System.Drawing.Size(97, 70);
             // 
-            // Cbb_Giaodich
+            // p2pToolStripMenuItem
             // 
-            this.Cbb_Giaodich.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
-            this.Cbb_Giaodich.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Cbb_Giaodich.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Cbb_Giaodich.ForeColor = System.Drawing.Color.White;
-            this.Cbb_Giaodich.FormattingEnabled = true;
-            this.Cbb_Giaodich.Items.AddRange(new object[] {
-            "Spot",
-            "P2p",
-            "Chuyển đổi"});
-            this.Cbb_Giaodich.Location = new System.Drawing.Point(152, 13);
-            this.Cbb_Giaodich.Name = "Cbb_Giaodich";
-            this.Cbb_Giaodich.Size = new System.Drawing.Size(78, 23);
-            this.Cbb_Giaodich.TabIndex = 1;
-            this.Cbb_Giaodich.Text = "Giao dịch";
+            this.p2pToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2pToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.p2pToolStripMenuItem.Name = "p2pToolStripMenuItem";
+            this.p2pToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.p2pToolStripMenuItem.Text = "P2p";
             // 
-            // button1
+            // spotToolStripMenuItem
             // 
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(236, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Futures";
-            this.button1.UseVisualStyleBackColor = true;
+            this.spotToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spotToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.spotToolStripMenuItem.Name = "spotToolStripMenuItem";
+            this.spotToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.spotToolStripMenuItem.Text = "Spot";
+            // 
+            // marginToolStripMenuItem
+            // 
+            this.marginToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.marginToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.marginToolStripMenuItem.Name = "marginToolStripMenuItem";
+            this.marginToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.marginToolStripMenuItem.Text = "Margin";
+            // 
+            // panel_Thitruong
+            // 
+            this.panel_Thitruong.Location = new System.Drawing.Point(2, 45);
+            this.panel_Thitruong.Name = "panel_Thitruong";
+            this.panel_Thitruong.Size = new System.Drawing.Size(798, 407);
+            this.panel_Thitruong.TabIndex = 1;
+            // 
+            // panel_future
+            // 
+            this.panel_future.Controls.Add(this.dgv_Thitruong);
+            this.panel_future.Location = new System.Drawing.Point(2, 45);
+            this.panel_future.Name = "panel_future";
+            this.panel_future.Size = new System.Drawing.Size(798, 407);
+            this.panel_future.TabIndex = 2;
+            // 
+            // dgv_Thitruong
+            // 
+            this.dgv_Thitruong.AllowUserToOrderColumns = true;
+            this.dgv_Thitruong.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
+            this.dgv_Thitruong.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv_Thitruong.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_Thitruong.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_Thitruong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv_Thitruong.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_Thitruong.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(47)))), ((int)(((byte)(51)))));
+            this.dgv_Thitruong.Location = new System.Drawing.Point(3, 3);
+            this.dgv_Thitruong.Name = "dgv_Thitruong";
+            this.dgv_Thitruong.RowHeadersVisible = false;
+            this.dgv_Thitruong.Size = new System.Drawing.Size(792, 390);
+            this.dgv_Thitruong.TabIndex = 0;
+            this.dgv_Thitruong.UseWaitCursor = true;
+            this.dgv_Thitruong.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgv_Thitruong_CellFormatting);
             // 
             // Trangchu
             // 
@@ -110,12 +206,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_future);
+            this.Controls.Add(this.panel_Thitruong);
             this.Controls.Add(this.Taskbar);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Trangchu";
             this.Text = "Trang chủ";
             this.Taskbar.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuGiaoDich.ResumeLayout(false);
+            this.panel_future.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Thitruong)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -125,7 +226,14 @@
         private System.Windows.Forms.Panel Taskbar;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btn_thịtruong;
-        private System.Windows.Forms.ComboBox Cbb_Giaodich;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGiaoDich;
+        private System.Windows.Forms.ContextMenuStrip menuGiaoDich;
+        private System.Windows.Forms.ToolStripMenuItem p2pToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem spotToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem marginToolStripMenuItem;
+        private System.Windows.Forms.Button btn_future;
+        private System.Windows.Forms.Panel panel_Thitruong;
+        private System.Windows.Forms.Panel panel_future;
+        private System.Windows.Forms.DataGridView dgv_Thitruong;
     }
 }
--- Forms/Trangchu.cs
+++ Forms/Trangchu.cs
@@ -8,18 +8,151 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Thêm các thư viện này
+using System.Net.Http;
+using Newtonsoft.Json;
+
 namespace fightCrypto.Forms
 {
     public partial class Trangchu : Form
     {
+        // Tạo một HttpClient để tái sử dụng
+        private static readonly HttpClient client = new HttpClient();
+
         public Trangchu()
         {
             InitializeComponent();
+
+            // Hiện panel thị trường
+            this.panel_Thitruong.Visible = true;
+
+            // Gán sự kiện Load cho Form
+            this.Load += Trangchu_Load;
+
+            // --- THÊM DÒNG NÀY ĐỂ KHỬ FLICKER ---
+            typeof(DataGridView).InvokeMember(
+               "DoubleBuffered",
+               System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.SetProperty,
+               null,
+               this.dgv_Thitruong, // Tên DataGridView của bạn
+               new object[] { true });
+            // --- HẾT PHẦN THÊM ---
         }
 
+        // --- HÀM MỚI: LẤY DỮ LIỆU KHI FORM ĐƯỢC LOAD ---
+        private async void Trangchu_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Gọi hàm để lấy dữ liệu thị trường (Spot)
+                await LoadSpotMarketData();
+
+                // (Bạn sẽ tạo một hàm tương tự cho Future)
+                // await LoadFutureMarketData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu từ Binance: " + ex.Message);
+            }
+        }
+
+        private async Task LoadSpotMarketData()
+        {
+            // 1. GỌI API
+            // Đây là API lấy thông tin 24h cho TẤT CẢ các cặp Spot
+            string apiUrl = "https://api.binance.com/api/v3/ticker/24hr";
+            string jsonResponse = await client.GetStringAsync(apiUrl);
+
+            // 2. CHUYỂN ĐỔI JSON
+            // Chuyển đổi chuỗi JSON thành một danh sách các đối tượng
+            List<BinanceTicker> tickers = JsonConvert.DeserializeObject<List<BinanceTicker>>(jsonResponse);
+
+            // 3. HIỂN THỊ LÊN DATAGRIDVIEW
+            // (Giả sử bạn đã kéo dgv_Thitruong vào panel_Thitruong)
+
+            // Chỉ lấy 100 cặp đầu tiên cho nhẹ
+            this.dgv_Thitruong.DataSource = tickers.Take(100).ToList();
+        }
+
+        // === XÓA CÁC HÀM PAINT VÌ CHÚNG KHÔNG CẦN THIẾT ===
+        // private void panel_future_Paint(object sender, PaintEventArgs e) { }
+        // private void panel_Thitruong_Paint(object sender, PaintEventArgs e) { }
+
+
+        // ... code các nút bấm của bạn ...
         private void btn_thịtruong_Click(object sender, EventArgs e)
         {
-          this.Close();
+            this.panel_Thitruong.Visible = true;
         }
+
+        // (Bạn cần thêm 1 nút cho Future)
+        private void btn_future_Click(object sender, EventArgs e)
+        {
+            this.panel_Thitruong.Visible = false;
+            this.panel_future.Visible = true; // Hiện panel future
+        }
+
+        private void btnGiaoDich_Click(object sender, EventArgs e)
+        {
+            menuGiaoDich.Show(btnGiaoDich, 0, btnGiaoDich.Height);
+        }
+
+
+        private void dgv_Thitruong_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (this.dgv_Thitruong.Columns[e.ColumnIndex].Name == "PriceChangePercent")
+            {
+                if (e.Value != null)
+                {
+                    // 1. Lấy giá trị (là string)
+                    string stringValue = e.Value.ToString();
+
+                    // 2. Chuyển nó thành số
+                    double numericValue;
+                    if (double.TryParse(stringValue, out numericValue))
+                    {
+                        // 3. Đổi màu dựa trên giá trị
+                        if (numericValue > 0)
+                        {
+                            e.CellStyle.ForeColor = Color.LimeGreen; // Màu xanh
+                        }
+                        else if (numericValue < 0)
+                        {
+                            e.CellStyle.ForeColor = Color.FromArgb(255, 100, 100); // Màu đỏ
+                        }
+                        else
+                        {
+                            e.CellStyle.ForeColor = Color.White; // Màu trắng
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // --- LỚP HỖ TRỢ ĐỂ ĐỰNG DỮ LIỆU TỪ BINANCE ---
+    // (Bạn phải định nghĩa lớp này để Json hoạt động)
+    public class BinanceTicker
+    {
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("priceChange")]
+        public string PriceChange { get; set; }
+
+        [JsonProperty("priceChangePercent")]
+        public string PriceChangePercent { get; set; }
+
+        [JsonProperty("lastPrice")]
+        public string LastPrice { get; set; }
+
+        [JsonProperty("highPrice")]
+        public string HighPrice { get; set; }
+
+        [JsonProperty("lowPrice")]
+        public string LowPrice { get; set; }
+
+        [JsonProperty("volume")]
+        public string Volume { get; set; }
     }
 }
